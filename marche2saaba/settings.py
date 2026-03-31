32fb1ffd3409a191cac5fcc9e11db494f28b54b8
legaalassane0-cpu <legaalassane0@gmail.com>
2026-03-31 04:40:17 +0000

    Update settings.py with corrected indentation and content

--- marche2saaba/settings.py
+++ marche2saaba/settings.py
@@ -9,55 +9,55 @@ ALLOWED_HOSTS = ['*']
 # Applications installées
 INSTALLED_APPS = [
     'django.contrib.admin',
-        'django.contrib.auth',
-            'django.contrib.contenttypes',
-                'django.contrib.sessions',
-                    'django.contrib.messages',
-                        'django.contrib.staticfiles',
-                            'annonces',
-                            ]
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'annonces',
+]
 
-                            # Middleware
-                            MIDDLEWARE = [
-                                'django.middleware.security.SecurityMiddleware',
-                                    'django.contrib.sessions.middleware.SessionMiddleware',
-                                        'django.middleware.common.CommonMiddleware',
-                                            'django.middleware.csrf.CsrfViewMiddleware',
-                                                'django.contrib.auth.middleware.AuthenticationMiddleware',
-                                                    'django.contrib.messages.middleware.MessageMiddleware',
-                                                        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                                                        ]
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
-                                                        ROOT_URLCONF = 'marche2saaba.urls'
+ROOT_URLCONF = 'marche2saaba.urls'
 
-                                                        # Templates
-                                                        TEMPLATES = [
-                                                            {
-                                                                    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                                                                            'DIRS': [],
-                                                                                    'APP_DIRS': True,
-                                                                                            'OPTIONS': {
-                                                                                                        'context_processors': [
-                                                                                                                        'django.template.context_processors.debug',
-                                                                                                                                        'django.template.context_processors.request',
-                                                                                                                                                        'django.contrib.auth.context_processors.auth',
-                                                                                                                                                                        'django.contrib.messages.context_processors.messages',
-                                                                                                                                                                                    ],
-                                                                                                                                                                                            },
-                                                                                                                                                                                                },
-                                                                                                                                                                                                ]
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
-                                                                                                                                                                                                WSGI_APPLICATION = 'marche2saaba.wsgi.application'
+WSGI_APPLICATION = 'marche2saaba.wsgi.application'
 
-                                                                                                                                                                                                # Base de données
-                                                                                                                                                                                                DATABASES = {
-                                                                                                                                                                                                    'default': {
-                                                                                                                                                                                                            'ENGINE': 'django.db.backends.sqlite3',
-                                                                                                                                                                                                                    'NAME': BASE_DIR / 'db.sqlite3',
-                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                        }
+# Base de données
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-                                                                                                                                                                                                                        # Fichiers statiques & médias
-                                                                                                                                                                                                                        STATIC_URL = '/static/'
-                                                                                                                                                                                                                        MEDIA_URL = '/media/'
-                                                                                                                                                                                                                        MEDIA_ROOT = BASE_DIR / 'media'
+# Fichiers statiques & médias
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
