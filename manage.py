3c0431803b44b6234b77372c7f8ea215a455dbc4
legaalassane0-cpu <legaalassane0@gmail.com>
2026-03-31 06:38:13 +0000

    le manage

--- manage.py
+++ manage.py
@@ -4,11 +4,11 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marche2saaba.settings')
-        try:
-                from django.core.management import execute_from_command_line
-                    except ImportError:
-                            raise
-                                execute_from_command_line(sys.argv)
+    try:
+        from django.core.management import execute_from_command_line
+            except ImportError:
+                    raise
+                        execute_from_command_line(sys.argv)
 
-                                if __name__ == '__main__':
-                                    main()
+                         if __name__ == '__main__':
+                            main()
