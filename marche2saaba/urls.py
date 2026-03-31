e3978ad28ed8309b54cee1b708452d15bd38ab5b
legaalassane0-cpu <legaalassane0@gmail.com>
2026-03-31 05:13:47 +0000

    Update urls.py with new routing configurations

--- marche2saaba/urls.py
+++ marche2saaba/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-        path('', include('annonces.urls')),  # routes de l'app annonces
-        ]
+    path('', include('annonces.urls')),  # routes de l'app annonces
+]
 
-        if settings.DEBUG:
-            urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
